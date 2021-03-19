d25540ff86d6b9f60615d787aac8d8e917d1420a
hyeonic <evan3566@gmail.com>
2021-03-19 16:33:28 +0900

    feat: initial commit
    개발환경 설정

---
+++ src/test/java/me/hyeonic/githubissue/GithubIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.githubissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
